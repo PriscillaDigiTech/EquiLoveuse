333c2399c0a642b44f325d889a36244339641a52
Priscilla DigiTech <priscilladigitech@gmail.com>
2024-05-12 06:34:49 +0200

    12/05/24 V1

---
+++ assets/js/index.js
@@ -0,0 +1,23 @@
+//HEADER
+
+const openIcon = document.getElementById("openIcon");
+const closeIcon = document.getElementById("closeIcon");
+const menuNav = document.getElementById("menuNav");
+
+//Au chargement de la page menu fermé
+
+function windowLoad() {
+  menuNav.style.display = "none";
+}
+
+//openMenu() = ouvrir le menu de navigation
+
+function openMenu() {
+  menuNav.style.display = "block";
+}
+
+//closeMenu() = ouvrir le menu de navigation
+
+function closeMenu() {
+  menuNav.style.display = "none";
+}
